6508df4009d3b5d0f91ad366765676fbbf9874ae
chuangui.xiecg <chuangui.xiecg@alibaba-inc.com>
2013-08-25 16:44:50 +0800

    map update

---
+++ static/js/pages/map.js
@@ -0,0 +1,84 @@
+
+jQuery(function($){
+
+	renderMap();
+    function renderMap(){
+	
+	
+        if(!$('#map_container')[0])return;
+		
+		
+        var map = new BMap.Map("map_container");
+        map.centerAndZoom(new BMap.Point(108.403765, 36.914850), 5);
+		
+		
+		function getBoundaryDigui(res){
+			var data = res.pop();
+            var bdary = new BMap.Boundary();
+            bdary.get(data.split("-")[0], function(rs){
+
+                var maxNum = -1, maxPly;
+                var color = data.split("-")[1];
+
+                var count = rs.boundaries.length;
+                for(var i = 0; i < count; i++){
+                    var ply = new BMap.Polygon(rs.boundaries[i], {strokeWeight: 1, strokeOpacity:0.5,fillColor:color,strokeColor: "#000000"});
+                    map.addOverlay(ply);
+
+                    //开始用"mouseover","mouseout"发现,鼠标移动过快,会多个省份也高亮了.所以改成 click了
+                    ply.addEventListener("click", function (e) {
+						return;
+                        var latlng = e.point;
+                        var info = new BMap.InfoWindow(name + " " + latlng.lat + "," + latlng.lng, {width:220});
+                        map.openInfoWindow(info, latlng);
+
+                        //高亮闪烁显示鼠标点击的省
+                        delay = 0;
+                        for (flashTimes = 0; flashTimes < 3; flashTimes++) {
+                            delay += 200;
+                            setTimeout(function () {
+                                ply.setFillColor("#FFFF00");
+                            }, delay);
+
+                            delay += 200;
+                            setTimeout(function () {
+                                ply.setFillColor(color);
+                            }, delay);
+                        }
+                    });
+                }
+                if(maxPly){
+                    map.setViewport(maxPly.getPoints());
+                }
+				
+				if(res.length>0)
+				
+				getBoundaryDigui(res);
+				
+            });
+        }
+
+        $.ajax({
+            url:$('#map_container').data('url'),
+            type:'GET',
+			success:function(){
+				
+			}
+        }).done(function(res){
+				res = jQuery.parseJSON(res);
+				if(res&&res.success){
+					getBoundaryDigui(res.data);
+					map.clearOverlays();
+					map.disableDragging();
+					map.disableScrollWheelZoom();
+					map.disableDoubleClickZoom();
+					map.disableKeyboard();
+					map.disableContinuousZoom();
+					map.disablePinchToZoom();
+				
+				}	
+               
+        });
+	}
+
+});
